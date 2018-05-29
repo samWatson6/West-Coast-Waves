4af69bd46954b75f9a8f615c2c485ebbc12951b8
Sam Watson <Sam.Watson3775@gmail.com>
2018-05-29 15:01:20 -0700

    Create mock locations

---
+++ src/app/mock-locations.ts
@@ -0,0 +1,9 @@
+import { Location } from './Location';
+
+export const LOCATIONS: Location[] = [
+  { id: 11, name: 'Pacifica' },
+  { id: 12, name: 'Mavericks' },
+  { id: 13, name: 'Fort Point' },
+  { id: 14, name: 'Santa Monica' },
+  { id: 15, name: 'WESTPORT' },
+];
