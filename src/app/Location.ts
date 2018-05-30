35cf32237c4e46194af6ef1c990d06ed790fbf54
Sam Watson <Sam.Watson3775@gmail.com>
2018-05-29 17:39:58 -0700

    Add chart to location object and template

--- src/app/Location.ts
+++ src/app/Location.ts
@@ -1,4 +1,5 @@
 export class Location {
   id: string;
   name: string;
+  chart: string;
 }
--- src/app/mock-locations.ts
+++ src/app/mock-locations.ts
@@ -1,11 +1,11 @@
 import { Location } from './Location';
 
 export const LOCATIONS: Location[] = [
-  { id: 'CA', name: 'Pacifica' },
-  { id: 'CA', name: 'Mavericks' },
-  { id: 'CA', name: 'Fort Point' },
-  { id: 'CA', name: 'Santa Monica' },
-  { id: 'WA', name: 'West Port' },
-  { id: 'WA', name: 'La Push' },
-  { id: 'OR', name: 'Cannon Beach' },
+  { id: 'CA', name: 'Pacifica', chart: 'http://cdn.magicseaweed.com/wave/750/1-1366902000-1.gif'},
+  { id: 'CA', name: 'Mavericks', chart: "http://cdn.magicseaweed.com/wave/750/1-1366902000-2.gif" },
+  { id: 'CA', name: 'Fort Point', chart: "http://cdn.magicseaweed.com/gfs/750/1-1366902000-4.gif" },
+  { id: 'CA', name: 'Santa Monica', chart: 'http://cdn.magicseaweed.com/wave/750/1-1366902000-1.gif' },
+  { id: 'WA', name: 'West Port', chart: "http://cdn.magicseaweed.com/wave/750/1-1366902000-2.gif"},
+  { id: 'WA', name: 'La Push', chart: 'http://cdn.magicseaweed.com/wave/750/1-1366902000-1.gif' },
+  { id: 'OR', name: 'Cannon Beach', chart: "http://cdn.magicseaweed.com/gfs/750/1-1366902000-4.gif" },
 ];
