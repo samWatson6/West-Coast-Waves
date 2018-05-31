9b68f92441fe07a0ea6293eac3473d4e7eea5ebc
Sam Watson <Sam.Watson3775@gmail.com>
2018-05-30 18:37:24 -0700

    Insert scripts to create and seed DB and add influx connection

--- server/server.js
+++ server/server.js
@@ -10,12 +10,16 @@ app.use(bodyParser.json());
 
 
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src/app'));
 app.use(express.static(__dirname + '/../node_modules'));
 
-app.use('/');
 
-app.listen(4200, function() {
-  console.log('listening on port 8000');
+  ;
+
+app.listen(8080, function() {
+  console.log('listening on port 8080');
 });
 
+
+
+module.exports.apps = app;
